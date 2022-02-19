3fc5f93d39a853d46622cbfa6b3c9482a0176082
HyeonSoo-Jeon <sop369@kakao.com>
2022-02-19 13:02:59 +0900

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText mAnswer;
+    private EditText userName;
+    private TextView titleHi;
+    private LinearLayout userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +27,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
 
 
-        mAnswer = (EditText) findViewById(R.id.input);
+        userName = (EditText) findViewById(R.id.input);
         Button mButton = (Button) findViewById(R.id.answer);
+        titleHi = findViewById(R.id.title_hi);
+        userInput = findViewById(R.id.userName);
 
         findViewById(R.id.answer).setOnClickListener(this);
 
+        Animation moveTop = AnimationUtils.loadAnimation(this,R.anim.title_trans);
+        Animation fadeIn = AnimationUtils.loadAnimation(this,R.anim.fadein);
+        moveTop.setInterpolator(AnimationUtils.loadInterpolator(this,android.R.anim.accelerate_decelerate_interpolator));
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                titleHi.startAnimation(moveTop);
+            }
+        }, 500);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                userInput.setVisibility(View.VISIBLE);
+                userInput.startAnimation(fadeIn);
+            }
+        }, 1000);
     }
 
 
@@ -33,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.answer:
                 String answer = "hi";
-                String userInput = mAnswer.getText().toString();
+                String userInput = userName.getText().toString();
                 int answerCount = 0;
 
                 if(answer.equals(userInput)) {
